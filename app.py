baca79855800263ba1c8129a53917a33477fd696
shrutidevops <kumarishruti1006@gmail.com>
2026-05-24 13:18:02 +0530

    fix: app_name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Shruti's CI/CD pipeline!", 200
+    return "Hello from Shruti's CI/CD pipeline2!", 200
 
 
 @app.route("/health")
